c1ae7751dfc6757c27c8e965302ae6c12223eab7
Eridiah <eridiahmods@gmail.com>
2016-12-17 12:23:06 -0500

    Added Recipes for Unbreakable Stone Tools
    In this commit, I added all of the recipes for the Unbreakable stone
    tools. Currently, I have the stone and wood tools finished, with only
    the iron, gold, and diamond tools to go.

--- java/net/unbreakabletools/mod/init/ModCrafting.java
+++ java/net/unbreakabletools/mod/init/ModCrafting.java
@@ -13,6 +13,13 @@ public class ModCrafting {
 		GameRegistry.addRecipe(new ItemStack(ModItems.godwoodenpickaxe), "DND", "DPD", "DDD", 'D', Items.DIAMOND, 'N', Items.NETHER_STAR, 'P', Items.WOODEN_PICKAXE);
 		GameRegistry.addRecipe(new ItemStack(ModItems.godwoodenaxe), "DND", "DAD", "DDD", 'A', Items.WOODEN_AXE, 'D', Items.DIAMOND, 'N', Items.NETHER_STAR);
 		GameRegistry.addRecipe(new ItemStack(ModItems.godwoodenhoe), "DND", "DHD", "DDD", 'D', Items.DIAMOND, 'H', Items.WOODEN_HOE, 'N', Items.NETHER_STAR);
+		
+		//Stone Tools
+		GameRegistry.addRecipe(new ItemStack(ModItems.godstonesword), "DND", "DSD", "DDD", 'D', Items.DIAMOND, 'N', Items.NETHER_STAR, 'S', Items.STONE_SWORD);
+		GameRegistry.addRecipe(new ItemStack(ModItems.godstoneshovel), "DND", "DSD", "DDD", 'D', Items.DIAMOND, 'N', Items.NETHER_STAR, 'S', Items.STONE_SHOVEL);
+		GameRegistry.addRecipe(new ItemStack(ModItems.godstonepickaxe), "DND", "DPD", "DDD", 'D', Items.DIAMOND, 'N', Items.NETHER_STAR, 'P', Items.STONE_PICKAXE);
+		GameRegistry.addRecipe(new ItemStack(ModItems.godstoneaxe), "DND", "DAD", "DDD", 'D', Items.DIAMOND, 'N', Items.NETHER_STAR, 'A', Items.STONE_AXE);
+		GameRegistry.addRecipe(new ItemStack(ModItems.godstonehoe), "DND", "DHD", "DDD", 'D', Items.DIAMOND, 'N', Items.NETHER_STAR, 'H', Items.STONE_HOE);
 	}
 	
 }
